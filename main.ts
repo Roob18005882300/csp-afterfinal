914837abdec69501b22f531903a6f44684e3f180
Roob18005882300 <80698127+Roob18005882300@users.noreply.github.com>
2022-05-10 10:17:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,73 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Jump_CNT == 0) {
+        PlayerChar.setVelocity(0, -250)
+        Jump_CNT += 1
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    PlayerChar.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        e e e e e e e e e e e e e e . . 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        a a a a e e e e e e e e e e 2 . 
+        a a a a a a a a a a a a a e . 2 
+        a a a a a a a a a a a a a e 2 2 
+        a a a a a a a a a a a a a a 2 2 
+        a a a a a a a a a a a a a c 2 . 
+        a a a a a a a a a a a a c c 2 . 
+        a a a a a a a a a a a c c a . . 
+        a a a a a a a a a a a c a c . . 
+        a a c c a a a a a a a c a c . . 
+        c c a a c c c c a a c a c a . . 
+        c c c c c c c c c c c c c c . . 
+        c c c c c c c c c c c c c c . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    PlayerChar.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . e e e e e e e e e e e e e e 
+        . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        . 2 e e e e e e e e e e a a a a 
+        2 . e a a a a a a a a a a a a a 
+        2 2 e a a a a a a a a a a a a a 
+        2 2 a a a a a a a a a a a a a a 
+        . 2 c a a a a a a a a a a a a a 
+        . 2 c c a a a a a a a a a a a a 
+        . . a c c a a a a a a a a a a a 
+        . . c a c a a a a a a a a a a a 
+        . . c a c a a a a a a a c c a a 
+        . . a c a c a a c c c c a a c c 
+        . . c c c c c c c c c c c c c c 
+        . . c c c c c c c c c c c c c c 
+        `)
+})
+let Jump_CNT = 0
+let PlayerChar: Sprite = null
+tiles.setCurrentTilemap(tilemap`BeginnerLvl`)
+PlayerChar = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . e e e e e e e e e e e e e e 
+    . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    . 2 e e e e e e e e e e a a a a 
+    2 . e a a a a a a a a a a a a a 
+    2 2 e a a a a a a a a a a a a a 
+    2 2 a a a a a a a a a a a a a a 
+    . 2 c a a a a a a a a a a a a a 
+    . 2 c c a a a a a a a a a a a a 
+    . . a c c a a a a a a a a a a a 
+    . . c a c a a a a a a a a a a a 
+    . . c a c a a a a a a a c c a a 
+    . . a c a c a a c c c c a a c c 
+    . . c c c c c c c c c c c c c c 
+    . . c c c c c c c c c c c c c c 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(PlayerChar)
+controller.moveSprite(PlayerChar, 100, 0)
+Jump_CNT = 0
+forever(function () {
+    PlayerChar.ay = 1150
+})
