ced05851205b9c724a85aea532d3cf2887c08d85
Roob18005882300 <80698127+Roob18005882300@users.noreply.github.com>
2022-05-12 09:43:21 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
+namespace SpriteKind {
+    export const Player_Cos = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Jump_CNT == 0) {
         PlayerChar.setVelocity(0, -250)
         Jump_CNT += 1
     }
 })
+function JumpInhibit () {
+    if (Jump_CNT >= 1) {
+        pause(500)
+        Jump_CNT = 0
+    }
+}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     PlayerChar.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -22,6 +31,16 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         c c a a c c c c a a c a c a . . 
         c c c c c c c c c c c c c c . . 
         c c c c c c c c c c c c c c . . 
+        `)
+    HeadBand_COS.setImage(img`
+        . . . . . . . . 
+        . . . . . . . . 
+        . . . 2 2 2 2 . 
+        . . 2 . . . . . 
+        2 2 2 2 2 2 2 . 
+        2 2 2 . . . . 2 
+        . . . . . . . . 
+        . . . . . . . . 
         `)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -43,8 +62,22 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . c c c c c c c c c c c c c c 
         . . c c c c c c c c c c c c c c 
         `)
+    HeadBand_COS.setImage(img`
+        . . . . . . . . 
+        . . . . . . . . 
+        . 2 2 2 2 . . . 
+        . . . . . 2 . . 
+        . 2 2 2 2 2 2 2 
+        2 . . . . 2 2 2 
+        . . . . . . . . 
+        . . . . . . . . 
+        `)
 })
+function FollowController () {
+    HeadBand_COS.setPosition(PlayerChar.x - 9, PlayerChar.y - 5)
+}
 let Jump_CNT = 0
+let HeadBand_COS: Sprite = null
 let PlayerChar: Sprite = null
 tiles.setCurrentTilemap(tilemap`BeginnerLvl`)
 PlayerChar = sprites.create(img`
@@ -65,9 +98,21 @@ PlayerChar = sprites.create(img`
     . . c c c c c c c c c c c c c c 
     . . c c c c c c c c c c c c c c 
     `, SpriteKind.Player)
+HeadBand_COS = sprites.create(img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . 2 2 2 2 . . . 
+    . . . . . . . 2 . . 
+    . . . 2 2 2 2 2 2 2 
+    . . 2 . . . . 2 2 2 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    `, SpriteKind.Player_Cos)
 scene.cameraFollowSprite(PlayerChar)
 controller.moveSprite(PlayerChar, 100, 0)
 Jump_CNT = 0
 forever(function () {
+    JumpInhibit()
+    FollowController()
     PlayerChar.ay = 1150
 })
